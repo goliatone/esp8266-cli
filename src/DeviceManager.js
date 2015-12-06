7d9601dbe71751fbdbef0e8a07644242a8c39d59
goliatone <burgosemi@gmail.com>
2015-12-06 00:12:37 -0500

    DeviceManager adding extra commands, info, etc

--- src/DeviceManager.js
+++ src/DeviceManager.js
@@ -1,21 +1,62 @@
+'use strict';
 
-
-
-var Promise = require('es6-promise').Promise;
-
-
+var Promise = require('bluebird');
 
 var CHUNK_SIZE = 128;
-
-
-
 
 function DeviceManager (comms) {
 	this._comms = comms;
 }
 
+DeviceManager.prototype.fsinfo = function(){
+	//TODO: Should we print("["..t..","..u..","..r.."]")
+	var command = 'r,u,t=file.fsinfo() print("Total : "..t.." bytes_NL_Used  : "..u.." bytes_NL_Remain: "..r.." bytes_NL_") r=nil u=nil t=nil';
+	return this._sendCommand(command)
+	.then(function (data) {
+		return '' + data.replace(/_NL_/gm, '\r\n');
+	});
+};
 
+DeviceManager.prototype.wifiIP = function(){
+	var command = 'if wifi.getmode() == 1 then print(wifi.sta.getip()) else print(wifi.ap.getip()) end';
+	return this._sendCommand(command);
+};
 
+DeviceManager.prototype.wifiRestore = function(){
+	var command = 'node.restore()';
+	return this._sendCommand(command);
+};
+
+DeviceManager.prototype.infoHeap = function(){
+	var command = 'print(node.heap())';
+	return this._sendCommand(command);
+};
+
+DeviceManager.prototype.infoFlashId = function(){
+	var command = 'print(node.flashid())';
+	return this._sendCommand(command);
+};
+
+DeviceManager.prototype.infoInfo = function(){
+	// majorVer (number)
+	// minorVer (number)
+	// devVer (number)
+	// chipid (number)
+	// flashid (number)
+	// flashsize (number)
+	// flashmode (number)
+	// flashspeed (number)
+	var command = 'ma, mi, de, ch, fl, fs, fm, fp = node.info();print("["..ma..","..mi..","..de..","..ch..","..fl..","..fs..","..fm..","..fp.."]");';
+	// var command = 'ma, mi, de, ch, fl, fs, fm, fp = node.info();print("["..ma..","..mi..","..de..","..ch..","..fl..","..fs..","..fm..","..fp.."]");ma=nil, mi=nil, de=nil, ch=nil, fl=nil, fs=nil, fm=nil, fp=nil';
+	return this._sendCommand(command).then(function(data){
+		return data.split('\t');
+	});
+};
+
+DeviceManager.prototype.infoChipId = function(){
+	var command = 'print(node.chipid())';
+	return this._sendCommand(command);
+};
 
 DeviceManager.prototype.getFileList = function () {
 	var command = 'for f,s in pairs(file.list()) do print(f,s) end';
@@ -33,10 +74,7 @@ DeviceManager.prototype.getFileList = function () {
 
 			return result;
 		});
-}
-
-
-
+};
 
 DeviceManager.prototype.removeFile = function (filename) {
 	var command = 'file.remove"' + filename + '"';
@@ -47,9 +85,6 @@ DeviceManager.prototype.removeFile = function (filename) {
 		});
 };
 
-
-
-
 DeviceManager.prototype.writeFile = function (filename, data) {
 	return this.removeFile(filename)
 		.then(this._writeFileHeader.bind(this, filename))
@@ -57,16 +92,10 @@ DeviceManager.prototype.writeFile = function (filename, data) {
 		.then(this._writeFileFooter.bind(this));
 };
 
-
-
-
 DeviceManager.prototype._writeFileHeader = function (filename, data) {
 	var command = 'file.open("' + filename + '", "w")';
 	return this._sendCommand(command);
 };
-
-
-
 
 DeviceManager.prototype._writeFileData = function (data) {
 	var _this = this,
@@ -92,9 +121,6 @@ DeviceManager.prototype._writeFileData = function (data) {
 	});
 };
 
-
-
-
 DeviceManager.prototype._writeFileChunk = function (chunk) {
 	var command,
 		translate = { '\t': '\\t', '\n': '\\n', '\r': '\\r', '"': '\\"', '\\': '\\\\' };
@@ -105,47 +131,30 @@ DeviceManager.prototype._writeFileChunk = function (chunk) {
 	return this._sendCommand(command);
 };
 
-
-
-
 DeviceManager.prototype._writeFileFooter = function () {
 	var command = 'file.flush()file.close()';
 	return this._sendCommand(command);
 };
 
-
-
-
 DeviceManager.prototype.readFile = function (filename) {
-	var command = 'file.open("' + filename + '","r")for line in file.readline do print(line) end file.close()';
+	// var command = '_view = function() local _line;if file.open("' + filename + '","r") then print("--FileView start"); repeat _line = file.readline(); if (_line~=nil) then print(string.sub(_line,1,-2)); end until _line==nil; file.close(); print("--FileView done.") end end _view();_view=nil';
+	var command = 'file.open("' + filename + '","r")for line in file.readline do print(line) end file.close() line=nil';
 	return this._sendCommand(command);
 };
-
-
-
 
 DeviceManager.prototype.executeFile = function (filename) {
 	var command = 'dofile"' + filename + '"';
 	return this._sendCommand(command);
 };
 
-
-
-
 DeviceManager.prototype.executeLua = function (lua) {
 	return this._sendCommand(lua);
 };
-
-
-
 
 DeviceManager.prototype.restart = function (lua) {
 	var command = 'node.restart()';
 	return this._sendCommand(command);
 };
-
-
-
 
 DeviceManager.prototype._sendCommand = function (command) {
 	var _this = this;
@@ -155,8 +164,4 @@ DeviceManager.prototype._sendCommand = function (command) {
 	});
 };
 
-
-
-
 module.exports = DeviceManager;
-

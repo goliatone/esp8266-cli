5d3ab542e8f1ad18a06276619f3e40dd2b863aad
goliatone <burgosemi@gmail.com>
2015-12-05 23:05:31 -0500

    Added commander program

--- bin/cli.js
+++ bin/cli.js
@@ -1,191 +1,119 @@
 #!/usr/bin/env node
 
-var fs = require('fs'),
-	SerialComms = require('../src/SerialComms'),
-	DeviceManager = require('../src/DeviceManager'),
+'use strict';
 
+
+var program = require('commander'),
+	pkg = require('../package'),
+	commands = require('./cmds'),
 	PORT_FILENAME = __dirname + '/port.txt';
 
 
-
-
-var config = {
-
-
-	port: {
-
-		set: function (port) {
-			fs.writeFileSync(PORT_FILENAME, port);
-		},
-
-
-		get: function () {
-			console.log ('Port:', port || '[not set]');
-		}
-
-	},
-
-
-	file: {
-
-		list: function () {
-			new SerialComms(port).on('ready', function (comms) {
-				new DeviceManager(comms).getFileList()
-					.then(function (files) {
-						for (var i = 0, file; file = files[i]; i++) {
-							size = '' + file.size;
-							size = '        '.substr(size.length) + size;
-							console.log (size + ' bytes  ' + file.filename);
-						}
-					})
-					.then(comms.close.bind(comms));
-			});
-		},
-
-		
-		remove: function (filename) {
-			new SerialComms(port).on('ready', function (comms) {
-				new DeviceManager(comms).removeFile(filename)
-					.then(comms.close.bind(comms));
-			});
-		},
-
-		
-		write: function (filename, destination) {
-			var data = '' + fs.readFileSync(filename),
-				pathLib = require('path'),
-				basename = pathLib.basename(destination || filename);
-
-			new SerialComms(port).on('ready', function (comms) {
-				new DeviceManager(comms).writeFile(basename, data)
-					.then(comms.close.bind(comms));
-			});
-		},
-
-
-		push: function (filename, destination) {
-			var data = '' + fs.readFileSync(filename),
-				pathLib = require('path'),
-				basename = pathLib.basename(destination || filename),
-				match = filename.match(/\.([^\.]+)$/);
-
-			if (match) {
-				switch (match[1]) {
-					case 'lua':
-						data = require('luamin').minify(data);
-						break;
-
-					case 'html':
-						data = require('html-minifier').minify(data, {
-							removeComments: true,
-							collapseWhitespace: true,
-							removeRedundantAttributes: true,
-							minifyJS: true,
-							minifyCSS: true
-						});
-						break;
-
-					case 'js':
-						data = require('uglify-js').minify(data, {fromString: true}).code;
-						break;
-
-					case 'css':
-						data = (new (require('clean-css'))).minify(data).styles;
-						break;
-				}
-			}
-
-			new SerialComms(port).on('ready', function (comms) {
-				new DeviceManager(comms).writeFile(basename, data)
-					.then(comms.close.bind(comms));
-			});
-		},
-
-
-		read: function (filename) {
-			new SerialComms(port).on('ready', function (comms) {
-				new DeviceManager(comms).readFile(filename)
-					.then(function (data) { return data.replace(/\r\n\r\n/g, '\n'); })
-					.then(console.log)
-					.then(comms.close.bind(comms));
-			});
-		},
-
-
-		execute: function (filename) {
-			new SerialComms(port).on('ready', function (comms) {
-				new DeviceManager(comms).executeFile(filename)
-					.then(console.log)
-					.then(comms.close.bind(comms));
-			});
-		}
-
-	},
-
-
-	restart: function () {
-		new SerialComms(port).on('ready', function (comms) {
-			new DeviceManager(comms).restart()
-				.then(comms.close.bind(comms));
-		});
-	},
-
-
-	run: function (lua) {
-		new SerialComms(port).on('ready', function (comms) {
-			new DeviceManager(comms).executeLua(lua)
-				.then(console.log)
-				.then(comms.close.bind(comms));
-		});
-	},
-
-  monitor: function() {
-		console.log("Displaying output from port " + port + ".");
-		console.log("Press ^C to stop.\n");
-		
-		new SerialComms(port).on('ready', function (comms) {
-			comms.monitor();
-		});  	
-  }
-
-};
-
-
-function execute (config, args) {
-	var prop = config[args.shift()];
-	if (!prop) return false;
-
-	if (typeof prop == 'function') {
-		prop.apply(null, args);
-		return true;
-	}
-
-	return execute(prop, args)
-}
-
-
-
-
-
-
-var args = process.argv.slice(2),
-	port,
+var port = 'getport',
 	success;
 
-
-if (args[0] == 'port' && args[1] == 'set') {
-	// NOOP
-
-} else if (!fs.existsSync(PORT_FILENAME)) {
-	console.log ('Serial port not configured.\nPlease use "esp port set <port_address>" to configure.');
-	process.exit();
-
-} else {
-	port = '' + fs.readFileSync(PORT_FILENAME);
-}
+program
+	.option('-v, --verbose')
+	.version(pkg.version);
 
 
-success = execute(config, args);
+/**
+ * port
+ * - Get
+ * - Set
+ */
+program
+	.command('port <cmd> [port]')
+	.description('Manage serial port configuration. Valid <cmd>\'s: get|set')
+	.action(function(cmd, port, options){
+		//validate command
+		switch (cmd) {
+			case 'get':
+				commands.port.get();
+				break;
+			case 'set':
+				commands.port.set(port);
+				break;
+			default:
+				console.error('Wrong!')
+				process.exit(1);
+		}
+	});
 
-if (!success) console.log ('Invalid command.');
+program
+	.command('file <cmd> [filename] [destination]')
+	.description('Manage files. Valid <cmd>\'s:' +
+		'\n\tlist: Show a list all files in the board' +
+		'\n\tremove [filename]: Remove the file [filename] from the board' +
+		'\n\twrite [filename] [destination]: Write the local file [filename] as [destination] in the board' +
+		//TODO: Rename to compress
+		'\n\tpush [filename] [destination]: Comress and write the local file [filename] as [destination] in the board.' +
+		'\n\t\tIt will check for the file\'s extension and use an appropiate compressor.' +
+		'\n\t\tSupported filetypes are: lua, html, js, and css.' +
+		'\n\tread [filename]: Read [filename] from the board and show in stdout.' +
+		'\n\texecute [filename]: Execute [filename] in the board using "dofile".\n'
+	)
+	.action(function(cmd, filename, destination, options){
+		switch (cmd) {
+			case 'list':
+				commands.file.list();
+				break;
+			case 'remove':
+				commands.file.remove(filename);
+				break;
+			case 'write':
+				commands.file.write(filename, destination);
+				break;
+			case 'push':
+				commands.file.push(filename, destination);
+				break;
+			case 'read':
+				commands.file.read(filename, destination);
+				break;
+			case 'execute':
+				commands.file.execute(filename, destination);
+				break;
+			default:
+				console.error('Wrong!')
+				process.exit(1);
+		}
+	});
 
+program
+	.command('restart')
+	.description('Restarts the board.')
+	.action(function(){
+		commands.restart();
+	});
+
+program
+	.command('run [lua]')
+	.description('Runs the [lua] command')
+	.action(function(lua){
+		commands.run(lua);
+	});
+
+program
+	.command('monitor')
+	.description('Shows output from port ' + port + '.\n\nPress ^C to stop.')
+	.action(function(){
+		commands.monitor();
+	});
+
+// program
+// 	.command('repl')
+// 	.description('Starts an interactive REPL to send commands to the board')
+// 	.action(function(){
+// 		console.log('ESP >');
+// 	});
+
+/*
+ * Other commands?
+ */
+program
+    .command('*')
+    .action(function(cmd){
+      console.error('Unknown command:', cmd);
+    });
+
+program.parse(process.argv);

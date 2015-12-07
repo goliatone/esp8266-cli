4bb1ac876c3603c6b62ccf13cef8ee642c51cf57
goliatone <burgosemi@gmail.com>
2015-12-06 23:29:41 -0500

    Esptytool updatez. TODO: Handle python busy error

--- src/Espytool.js
+++ src/Espytool.js
@@ -4,6 +4,24 @@ var spawn = require('child_process').spawn;
 var join = require('path').join,
     resolve = require('path').resolve;
 
+/*
+We might get an error like this one, which means that
+the comm port is busy. Make it useful and easy to grok :)
+
+Traceback (most recent call last):
+  File ".../scripts/esptool.py", line 724, in <module>
+
+
+    main()
+  File ".../scripts/esptool.py", line 575, in main
+    esp = ESPROM(args.port, args.baud)
+  File ".../scripts/esptool.py", line 66, in __init__
+    self._port = serial.Serial(port)
+  File "build/bdist.macosx-10.9-x86_64/egg/serial/serialutil.py", line 282, in __init__
+  File "build/bdist.macosx-10.9-x86_64/egg/serial/serialposix.py", line 289, in open
+OSError: [Errno 16] Resource busy: '/dev/cu.SLAB_USBtoUART'
+*/
+
 /**
  * esptool.py --port=/dev/cu.SLAB_USBtoUART write_flash -fm=dio -fs=32m -ff=40m 0x00000 ../nodemcu_integer_0.9.6-dev_20150627.bin
  * @param  {String}   port     Comm port.
@@ -15,9 +33,9 @@ function write_flash (port, firmware, cb){
     var args = [scriptpath,
         '-p', port,
         'write_flash',
-        '-fm=dio',
-        '-fs=32m',
-        '-ff=40m',
+        // '-fm=dio',
+        // '-fs=32m',
+        // '-ff=40m',
         '0x00000',
         firmware //../nodemcu_integer_0.9.6-dev_20150627.bin
     ];
@@ -37,6 +55,9 @@ function write_flash (port, firmware, cb){
             if(code !== 0) reject(code);
             else resolve();
         });
+        command.on('error', function(err){
+            reject(err);
+        })
     });
 }
 
